7c7268b80fa2973c8bad78a6bbaf9a2f96955f86
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2023-08-06 23:11:00 +0530

    Create snip4.m

---
+++ snip4.m
@@ -0,0 +1,8 @@
+disp("To check whether (ab)'=a'b' ");
+mul = a * b;
+mulconj = mul'
+mulconj2 = aconj * bconj
+disp(" (ab)'=a'b' are not equal");
+
+% This snippet multiplies matrices a and b, calculates the transpose of their product, and also computes the product of their conjugate transposes. 
+% The output indicates that (ab)'=a'b' are not equal, which is generally true for matrix multiplication and its transpose.
