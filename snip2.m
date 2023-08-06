cb7b3fced18595643d1bc650489e596513bcbde9
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2023-08-06 23:09:16 +0530

    Create snip2.m

---
+++ snip2.m
@@ -0,0 +1,6 @@
+disp("to check what a'' is equal to");
+doubleconjugate = a''
+disp("Hence proved a''=a ");
+
+% This snippet calculates the double conjugate of the matrix a, which means taking the complex conjugate of a twice. 
+% It then prints the result and states that a''=a, which is true because taking the complex conjugate twice cancels out and gives the original matrix.
