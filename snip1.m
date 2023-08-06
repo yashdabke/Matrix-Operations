95e996ae530003614f6876661b40bd3987a4c841
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2023-08-06 23:05:54 +0530

    Create snip1.m

---
+++ snip1.m
@@ -0,0 +1,11 @@
+disp(" Defining a matrix ");
+a = [2+3i 7-8i; 5-i 2];
+disp("the complex conjugate of a is");
+conjugate = a'
+disp(" the conjugate transpose of the matrix a is");
+ctranspose(a)
+
+% This snippet defines a matrix a with complex entries and then computes its complex conjugate and conjugate transpose. 
+% The complex conjugate of a complex number a + bi is a - bi. 
+% The complex conjugate of a matrix involves taking the complex conjugate of each element. 
+% The conjugate transpose is the transpose of the complex conjugate of the matrix.
