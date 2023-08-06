f06d3caf4ccecbebd7f0b4f50425162a23f07575
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2023-08-06 23:11:32 +0530

    Create snip5.m

---
+++ snip5.m
@@ -0,0 +1,6 @@
+disp("To display the eigenvalues of the matrix");
+eigenval = eig(a)
+disp(" The eigenvalues are the diagonal elements for a Diagonal matrix ");
+
+% This snippet computes the eigenvalues of matrix a using the eig function and prints them.
+% It then states that for a diagonal matrix, the eigenvalues are equal to its diagonal elements.
